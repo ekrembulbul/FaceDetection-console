6339f21168b5c735ea23f0aa7f5fcbaab54fa8c1
ekrembulbul <l1115078@std.yildiz.edu.tr>
2019-07-19 15:01:36 +0300

    add face detect

---
+++ face_detection/face_detection.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+void detectFaceAndEye( const cv::Mat & src, cv::Mat & dst, cv::CascadeClassifier & eyeCascade, cv::CascadeClassifier & faceCascade );
+
+int main()
+{
+	const std::string eyeFileName = "haarcascade-eye.xml" ;
+	const std::string faceFileName = "haarcascade-frontalface-default.xml";
+	const std::string winName = "Image";
+
+	cv::CascadeClassifier eyeCascade;
+	cv::CascadeClassifier faceCascade;
+	eyeCascade.load( eyeFileName );
+	faceCascade.load( faceFileName );
+
+	cv::Mat frame = cv::imread( "m(01-32)_gr.jpg" );
+
+	cv::Mat outFrame;
+	detectFaceAndEye( frame, outFrame, eyeCascade, faceCascade );
+
+	cv::namedWindow( winName );
+	cv::imshow( winName, outFrame );
+	cv::waitKey( 0 );
+
+/*
+	cv::Mat frame;
+	cv::VideoCapture cap(0);
+	while ( true )
+	{
+		cap >> frame;
+		cv::namedWindow( "video" );
+		cv::imshow( "video", frame );
+		cv::waitKey( 40 );
+	}
+*/
+	return 0;
+}
+
+void detectFaceAndEye( const cv::Mat & src, cv::Mat & dst, cv::CascadeClassifier & eyeCascade, cv::CascadeClassifier & faceCascade )
+{
+	cv::Mat gray;
+	std::vector<cv::Rect> faces;
+
+	cv::cvtColor( src, gray, cv::COLOR_BGR2GRAY );
+	faceCascade.detectMultiScale( gray, faces );
+	dst = src;
+
+	for ( auto & face : faces )
+	{
+		cv::rectangle( dst, face, cv::Scalar( 0, 0, 255 ), 2 );
+	}
+}
